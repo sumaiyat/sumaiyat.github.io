7bf15f81868a4326a3877dc663840157577dc98a
Sumaiya T Z <59446073+sumaiyat@users.noreply.github.com>
2026-05-10 00:14:21 -0700

    Fix App.tsx router basename

--- frontend/lovable/src/App.tsx
+++ frontend/lovable/src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/lovable">
+      <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
